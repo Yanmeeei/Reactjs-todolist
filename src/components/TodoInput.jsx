024f37d532710e900616c3068718bf382fb69256
Yanmeeei <wyanmei@umich.edu>
2024-11-20 17:09:42 -0500

    enable enter key

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,16 +2,26 @@ import {useState} from "react";
 
 export default function TodoInput(props) {
     let {handleAddTodos, todoValue, setTodoValue} = props
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleAddTodos(todoValue);
+            setTodoValue("");
+        }
+    };
 
     return (
         <header>
-            <input value={todoValue} onChange={(e) => (
-                setTodoValue(e.target.value)
-            )} placeholder={"Enter todo item"} />
+            <input value={todoValue}
+                   onChange={(e) => (
+                       setTodoValue(e.target.value)
+                   )}
+                   onKeyDown={handleKeyDown}
+                   placeholder={"Enter todo item"}/>
             <button onClick={() => {
                 handleAddTodos(todoValue)
                 setTodoValue("")
-            }}>Add</button>
+            }}>Add
+            </button>
         </header>
     )
 }
